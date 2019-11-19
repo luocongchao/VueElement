bbfe97542c2a8c6efa5b16707425863517f42c8c
luocongchao <956578799@qq.com>
2019-11-19 18:21:34 +0800

    upload

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import app from './app.vue'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+import router from './router'
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, {
+  locale
+})
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+
+
+new Vue({
+  el: "#app",
+  router,
+  render: h => h(app),
+})
